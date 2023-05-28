5a384514233ba15e85cd3f6d17dae0bbed032306
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-05-29 01:17:19 +0800

    events

---
+++ events/new.js
@@ -0,0 +1,30 @@
+function doFirst(){
+  // 跟 HTML 產生關聯，再建立事件聆聽功能
+  // 關鍵是"表單＂,送出後抓裡面的值
+  document.querySelector('#theForm').onsubmit = setEvent
+}
+
+function setEvent(e){
+  e.preventDefault()
+
+  let events = ['contextmenu','select','change','input','scroll','resize']
+
+  for(let i = 0; i < events.length; i++){
+    // 先跟每個 checkbox 產生關聯
+    let yourCheck = document.getElementById(events[i])
+
+    // 如果有勾選，幫整個網頁建立該事件的聆聽功能
+    if(yourCheck.checked){
+      document.addEventListener(events[i], report)
+    }
+    
+  }
+  feedback = ''
+  // return false;
+}
+function report(e){
+  feedback += `${e.target.nodeName}: ${e.type} \n`
+  document.getElementById('feedback').value = feedback
+}
+
+window.addEventListener('load',doFirst)
