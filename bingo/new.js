3b570ffc285682a7e51eb68461d0864090863688
Jin22221 <117150611+Jin22221@users.noreply.github.com>
2023-06-09 11:29:08 +0800

    number no repeat

--- bingo/new.js
+++ bingo/new.js
@@ -1,9 +1,16 @@
 function doFirst(){
   let random = document.querySelectorAll('.random')
   // console.log(random);
-  
-  for(let i = 0; i < random.length; i++){
-    let randomNum = parseInt(Math.random() *24) + 1
+  let randomNum 
+  let usedNums = []
+  for(let i = 0; i < random.length; i++){  
+    do {
+      randomNum = parseInt(Math.random() * 24) + 1
+      // 先產生一個數字
+      // 如果 usedNums裡面有存在該數字，就繼續做
+    }while (usedNums.includes(randomNum))
+
+    usedNums.push(randomNum)
     random[i].innerHTML = randomNum
   }
     
